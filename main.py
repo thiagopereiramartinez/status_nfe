49d411c94340ac4995f05d97202fcc8000ca1cb2
Thiago <thiago_veloztecnologia@hotmail.com>
2019-12-03 11:25:59 -0400

    Commit inicial

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify
+from bs4 import BeautifulSoup
+from requests import get
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+
+    result = {}
+
+    # Requisitar página de status
+    request = get("http://www.nfe.fazenda.gov.br/portal/disponibilidade.aspx?versao=0.00&tipoConteudo=Skeuqr8PQBY=")
+    html = request.content
+
+    # Fazer o parse do HTML
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find(id="ctl00_ContentPlaceHolder1_gdvDisponibilidade2")
+
+    items = list()
+
+    for tr in table.find_all("tr"):
+        # Eliminar o cabeçalho
+        if tr.th != None:
+            continue
+
+        # Fazer parse do TD
+        info = list()
+
+        for td in tr.find_all("td"):
+            if td.img != None:
+                info.append(td.img["src"].split("_")[1])
+                continue
+
+            info.append(td.get_text())
+
+        items.append(info)
+
+    return jsonify({
+        "autorizadores": list(map(lambda x: {
+            "autorizador": x[0],
+            "autorizacao": x[1],
+            "retorno_autorizacao": x[2],
+            "inutilizacao": x[3],
+            "consulta_protocolo": x[4],
+            "status_servico": x[5],
+            "tempo_medio": x[6],
+            "consulta_cadastro": x[7],
+            "recepcao_evento": x[8]
+        }
+    , items)),
+        "meta": {
+            "ultima_verificacao": table.caption.span.get_text().split(" - ")[1].split(": ")[1],
+            "versao": table.caption.span.get_text().split(" - ")[2]
+        }
+    })
+
+if __name__ == "__main__":
+    os.system('sh run.sh')
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+env FLASK_APP=main.py flask run
